f69377f197803b118d4db2d6a043ef4950939660
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-08-25 11:33:21 +0200

    Startup

---
+++ ServerSideSocket/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSideSocket
+{
+    class Program
+    {
+        private bool stop;
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
